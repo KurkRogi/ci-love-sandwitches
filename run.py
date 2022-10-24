4efab4aa3525320876d71772bb120ffb079af593
KurkRogi <igor.k.kruk@gmail.com>
2022-10-24 20:51:37 +0000

    Add get_sales_data function collecting data from user input

--- run.py
+++ run.py
@@ -12,8 +12,21 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("love_sandwiches")
 
-sales = SHEET.worksheet('sales')
+# sales = SHEET.worksheet('sales')
+# data = sales.get_all_values()
+# print(data)
 
-data = sales.get_all_values()
+def get_sales_data():
+    """
+    Get sales data from the user
+    """
+    print("\n"+"-"*10)
+    print("Please provade sales data")
+    print("6 numbers separated by comas")
+    print("e.g.: 12,38,45,12,4,21\n")
 
-print(data)
+    data_str = input("Please enter your numbers and press Enter: ")
+
+    print(f"Data provided: {data_str}\n")
+
+get_sales_data()
